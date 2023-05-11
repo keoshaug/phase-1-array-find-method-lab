11a7170999e099f1fd33423af9a74b082e092547
Kevin OShaughnessy <kevinoshaughnessy@ymail.com>
2023-05-10 22:16:34 -0400

    finding array

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+function superbowlWin (array) {
+    if(array.find(obj => obj.result === 'W')) {
+        return array.find(obj => obj.result === 'W').year
+    } else {
+        return undefined
+    }
+}
